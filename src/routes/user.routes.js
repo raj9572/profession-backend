9558e9bd7f931832bfd9a02b8865f6bc1c07242a
rajali <rajali1432@gmail.com>
2024-02-28 15:58:21 +0530

    add get watch history pipeline

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -26,6 +26,7 @@ router.route("/login").post(loginUser)
 // secure routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken)
 
 
 
